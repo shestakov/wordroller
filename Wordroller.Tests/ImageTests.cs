81a9814485892197a2c4b06a5cab514f9c873a00
Alexander Shestakov <shestakov@hardroller.ru>
2021-08-25 17:28:27 +0500

    - Adding a test demonstrating image replacement.

--- Wordroller.Tests/ImageTests.cs
+++ Wordroller.Tests/ImageTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using Wordroller.Content.Images;
+using Wordroller.Content.Text.RunContent;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,6 +27,33 @@ namespace Wordroller.Tests
 			paragraph.AppendPicture(picture);
 
 			SaveTempDocument(document, "Image_Append.docx");
+		}
+
+		[Fact]
+		public void ReplaceImage()
+		{
+			using var documentWithoutImage = TestHelper.CreateNewDocument();
+			using var imageStream = TestHelper.GetResourceStream("Wordroller.Tests.Resources.Image_200x100.png");
+			var image = documentWithoutImage.AddImage(imageStream, KnownImageContentTypes.Png);
+
+			var section = documentWithoutImage.Body.Sections.First();
+			var picture = section.WrapImageIntoInlinePicture(image, "wordroller", "This is a sample image", 200, 100);
+			section.AppendParagraph().AppendPicture(picture);
+
+			using var writeStream = new MemoryStream();
+			documentWithoutImage.Save(writeStream);
+			var documentBytes = writeStream.GetBuffer();
+
+			using var readStream = new MemoryStream(documentBytes);
+			using var documentWithImage = new WordDocument(readStream);
+			var drawings = documentWithImage.Body.Content.AllParagraphsRecursive.SelectMany(p => p.Content.SelectMany(r => r.Content.OfType<RunDrawing>()));
+			var drawing = drawings.Single();
+			Assert.NotNull(drawing.InlinePicture);
+			var existingImage = drawing.InlinePicture!.Image;
+			using var newImageReadStream = TestHelper.GetResourceStream("Wordroller.Tests.Resources.Image_200x100_inverse.png");
+			using var newImageWriteStream = documentWithImage.GetImageStream(existingImage, FileMode.Create, FileAccess.Write);
+			newImageReadStream.CopyTo(newImageWriteStream);
+			SaveTempDocument(documentWithImage, "Image_Replace.docx");
 		}
 
 		[Fact]
